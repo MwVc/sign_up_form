4378c911054e22d6e69f865f6e6d1040c498b920
Victor Mwadime <victormwadime@gmail.com>
2023-06-30 13:30:54 +0300

    styling markup

---
+++ index.js
@@ -0,0 +1,10 @@
+//Confirm password validation
+const passwordOne = document.getElementById("password-one");
+const passwordTwo = document.getElementById("password-two");
+const errorEl = document.getElementById("error-el");
+const submitButton = document.getElementById("submit-button");
+const form = document.getElementById("form");
+
+function passwordValidation() {}
+
+passwordValidation();
